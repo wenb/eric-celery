9b135cfb2302e4f9be19d15f17db8fa5658a49b1
fenwenb <fengwenb@gmail.com>
2016-04-25 01:29:45 +0800

    change to pep8 format

--- task.py
+++ task.py
@@ -1,15 +1,24 @@
 from celery import Celery
-from celery.signals import task_success
+# from celery.signals import task_success
 import subprocess as sb
 
-app = Celery('task',backend='redis://serveric:32774/5',broker='amqp://guest@serveric:32771//')
+app = Celery(
+    'task',
+    backend='redis://serveric:32774/5',
+    broker='amqp://guest@serveric:32771//')
 
 app.config_from_object('celeryconfig')
 
+
 @app.task
 def run_command(command):
-	p = sb.Popen(command,stdin=sb.PIPE,stdout=sb.PIPE,stderr=sb.PIPE,shell=True)
-	out = p.stdout.readlines()
-	for line in out:
-		print line.strip()
-	return out
+    p = sb.Popen(
+        command,
+        stdin=sb.PIPE,
+        stdout=sb.PIPE,
+        stderr=sb.PIPE,
+        shell=True)
+    out = p.stdout.readlines()
+    for line in out:
+        print line.strip()
+    return out
