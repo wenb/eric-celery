24f766f1cdac2b4b7d8ae5bdc646a88115a67a39
fenwenb <fengwenb@gmail.com>
2016-04-25 01:04:50 +0800

    add main func

--- consume.py
+++ consume.py
@@ -1,19 +1,34 @@
-# encoding=utf8  
+# encoding=utf8
+import sys  
 from task import run_command
 from optparse import OptionParser
+
+class Usage(Exception):
+	"""docstring for Usage"""
+	def __init__(self, msg):
+		self.msg = msg
 
 def print_lines(out_put):
 	for line in out_put:
 		print line.strip()
 
 #set the paramater
-parser = OptionParser(usage="tran paramater", version="0.1")
-parser.add_option("-p","--paramater",dest="paramater",
-			help="tran paramater",default="echo \"hello world\"")
 
-(option, args) = parser.parse_args()
-command = option.paramater
+def main():
+	if not sys.argv[1:]:
+		print >> sys.stderr,"need a paramater"
+	try:
+		parser = OptionParser(usage="tran paramater", version="0.1")
+		parser.add_option("-p","--paramater",dest="paramater",
+			help="tran paramater",default="echo \"hello world\"")	
+		(option, args) = parser.parse_args()
+		command = option.paramater
+		result = run_command.delay(command)
+		print_lines(result.get(timeout=100))
+		#print command
+	except OptionParser.error, msg:
+		raise Usage(msg)
+		return 2
 
-#excute the command
-result = run_command.delay(command)
-print_lines(result.get(timeout=100))
+if __name__ == '__main__':
+	sys.exit(main())
