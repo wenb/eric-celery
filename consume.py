0f3dfd1edf089733fe4a79504052b6d32da8106a
fenwenb <fengwenb@gmail.com>
2016-04-25 00:07:21 +0800

    update

--- consume.py
+++ consume.py
@@ -1,3 +1,4 @@
+# encoding=utf8  
 from task import run_command
 from optparse import OptionParser
 
@@ -5,15 +6,14 @@ def print_lines(out_put):
 	for line in out_put:
 		print line.strip()
 
+#set the paramater
 parser = OptionParser(usage="tran paramater", version="0.1")
-
 parser.add_option("-p","--paramater",dest="paramater",
 			help="tran paramater",default="echo \"hello world\"")
 
 (option, args) = parser.parse_args()
-
 command = option.paramater
 
+#excute the command
 result = run_command.delay(command)
-#print result.get(timeout=1)
 print_lines(result.get(timeout=100))
--- task.py
+++ task.py
@@ -8,7 +8,7 @@ app.config_from_object('celeryconfig')
 
 @app.task
 def run_command(command):
-	p = sb.Popen(command,stdin=sb.PIPE,stdout=sb.PIPE,shell=True)
+	p = sb.Popen(command,stdin=sb.PIPE,stdout=sb.PIPE,stderr=sb.PIPE,shell=True)
 	out = p.stdout.readlines()
 	for line in out:
 		print line.strip()
