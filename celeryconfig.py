88da62e7cf189cbfafb784167066f14ab896ddc7
eric.feng <fengwenb@gmail.com>
2016-04-30 22:29:55 +0800

    add celery config in file

--- celeryconfig.py
+++ celeryconfig.py
@@ -2,3 +2,24 @@ CELERY_TIMEZONE = 'Asia/Shanghai'
 CELERY_ENABLE_UTC = True
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
+CELERY_ACCEPT_CONTENT = ['json']
+BROKER_URL = 'amqp://guest@serveric:32771//'
+CELERY_RESULT_BACKEND = 'redis://serveric:32774/5'
+BROKER_HEARTBEAT_CHECKRATE = '60s'
+
+# Enables error emails.
+# CELERY_SEND_TASK_ERROR_EMAILS = True
+n
+# # Name and email addresses of recipients
+# ADMINS = (
+#     ('eric feng', 'fengeric0910@163.com'),
+# )
+
+# # Email address used as sender (From field).
+# SERVER_EMAIL = 'fengeric0910@163.com'
+
+# # Mailserver configuration
+# EMAIL_HOST = 'mail.163.com'
+# EMAIL_PORT = 25
+# EMAIL_HOST_USER = 'fengeric0910@163.com'
+# EMAIL_HOST_PASSWORD = 'f1w9b88bb'
--- task.py
+++ task.py
@@ -1,11 +1,9 @@
 from celery import Celery
-# from celery.signals import task_success
+from celery.signals import task_success
 import subprocess as sb
 
 app = Celery(
-    'task',
-    backend='redis://serveric:32782/5',
-    broker='amqp://guest@serveric:32778//')
+    'task')
 
 app.config_from_object('celeryconfig')
 
@@ -24,3 +22,17 @@ def run_command(command):
         print line.strip()
     out.append(str(p.returncode))
     return out
+
+
+@app.task(bind=True)
+def error_handler(self, uuid):
+    result = self.app.AsyncResult(uuid)
+    print('Task {0} raised exception: {1!r}\n{2!r}'.format(
+          uuid, result.result, result.traceback))
+
+
+@task_success.connect()
+def handler_task_success(send=None, result=None, **kwargs):
+    print('task_success for result {result}'.format(
+        result=result,
+    ))
