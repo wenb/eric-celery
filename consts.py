2f7dd3b269e066c497b39d076e5dffed4dbacb2e
wenb <fengwenb@gmail.com>
2016-11-20 14:16:55 +0800

    fix bug in file

--- consts.py
+++ consts.py
@@ -5,6 +5,6 @@ USERNAME = 'root'
 PASSWORD = '123456'
 
 RABBITMQ_URI = 'amqp://admin:{}@{}:5672//'.format(PASSWORD, HOSTNAME)
-REDIS_URI = 'redis://{}:6379/0'.format()
+REDIS_URI = 'redis://{}:6379/0'.format(HOSTNAME)
 DB_URI = 'mysql://{}:{}@{}/{}'.format(
     USERNAME, PASSWORD, HOSTNAME, DATABASE)
